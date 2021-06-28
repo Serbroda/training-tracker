9357832089689205c061c8cf66422a05283502ee
Danny Rottstegge <danny@rottstegge.net>
2021-06-28 22:54:03 +0200

    First in

---
+++ training-tracker-webapp/src/test/java/de/rottstegge/trainingtracker/TrainingTrackerWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rottstegge.trainingtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingTrackerWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
