3dd202a2ef73c60bcf3b48b15ede68f9677c710c
Danny Rottstegge <danny@rottstegge.net>
2021-07-20 19:12:16 +0200

    Add font-awesome

--- training-tracker-client/src/app/app.module.ts
+++ training-tracker-client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { BASE_PATH } from 'training-tracker-sdk-angular';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,7 +14,7 @@ import { SettingsComponent } from './pages/settings/settings.component';
 
 @NgModule({
     declarations: [AppComponent, SidenavComponent, DashboardComponent, SettingsComponent],
-    imports: [HttpClientModule, BrowserModule, AppRoutingModule],
+    imports: [HttpClientModule, BrowserModule, AppRoutingModule, FontAwesomeModule],
     providers: [{ provide: BASE_PATH, useValue: environment.apiBaseUrl }],
     bootstrap: [AppComponent],
 })
--- training-tracker-client/src/app/pages/dashboard/dashboard.component.ts
+++ training-tracker-client/src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { faChartBar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+    faChartBar = faChartBar;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
