1225a785bdd8944ce46fbdd789543f6704270399
Danny Rottstegge <danny@rottstegge.net>
2021-07-20 16:43:45 +0200

    Add tailwindcss

--- training-tracker-client/src/app/app.module.ts
+++ training-tracker-client/src/app/app.module.ts
@@ -4,20 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {BASE_PATH} from 'training-tracker-sdk-angular';
-import {HttpClientModule} from '@angular/common/http';
+import { BASE_PATH } from 'training-tracker-sdk-angular';
+import { HttpClientModule } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    HttpClientModule,
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [{provide: BASE_PATH, useValue: environment.apiBaseUrl}],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, SidenavComponent],
+    imports: [HttpClientModule, BrowserModule, AppRoutingModule],
+    providers: [{ provide: BASE_PATH, useValue: environment.apiBaseUrl }],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
